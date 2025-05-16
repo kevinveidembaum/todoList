3b42f2dd20d13a0d1b6c668dd97346b1786ddb7a
Kevin Nikolai <nikolai10052005@gmail.com>
2025-05-16 16:18:09 -0300

    fix: utc and authorization error

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/new_task', [TaskController::class, 'store']);
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
